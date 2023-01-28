b7664368d117a534e9dcfc69eddfbdd8e99ea47d
MichaelSciole412 <scioleme@appstate.edu>
2023-01-28 12:10:44 -0500

    Updated App.java to allow players to make custom classes...what fun

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 abstract class Player{
@@ -59,15 +60,90 @@ class Raider extends Player {
     }
   }
 
+  class Custom extends Player
+  {
+    public void setStats() 
+    {
+      int skillPoints = 21;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Please set your intellegence: ");
+      intellegence = setSkill(skillPoints, sc);
+      skillPoints = skillPoints - intellegence;
+      System.out.println("Skill points left to spend: " + skillPoints);
+      if(skillPoints == 0)
+      {
+        return;
+      }
+
+      System.out.println("Please set your strength: ");
+      strength = setSkill(skillPoints, sc);
+      skillPoints = skillPoints - strength;
+      System.out.println("Skill points left to spend: " + skillPoints);
+      if(skillPoints == 0)
+      {
+        return;
+      }
+
+      System.out.println("Please set your combat: ");
+      combat = setSkill(skillPoints, sc);
+      skillPoints = skillPoints - combat;
+      System.out.println("Skill points left to spend: " + skillPoints);
+      if(skillPoints == 0)
+      {
+        return;
+      }
+
+      System.out.println("Please set your charisma: ");
+      charisma = setSkill(skillPoints, sc);
+      skillPoints = skillPoints - charisma;
+      System.out.println("Skill points left to spend: " + skillPoints);
+      if(skillPoints == 0)
+      {
+        return;
+      }
+
+      sc.close();
+    }
+
+
+    public int setSkill(int sp, Scanner getskill)
+    {
+      int skill = -1;
+      while (skill > sp || skill < 0)
+      {
+        try
+        {
+          skill = getskill.nextInt();
+        }
+        catch (InputMismatchException e)
+        {
+          getskill.nextLine();
+          System.out.println("Invalid input!");
+        }
+        if (skill > 10 || skill < 0)
+        {
+          System.out.println("Select a number between 0 and 10!");
+          skill = -1;
+        }
+      }
+      return skill;
+    }
+
+    public void printStats()
+    {
+      System.out.println("Custom Stats:\nIntellegence: " + intellegence + "\nStrength: " + strength + "\nCombat: " + combat + "\nCharisma: " + charisma);
+    }
+  }
+
 
 public class App{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     Player player1;
-    System.out.println("What would you like to be in this crazy new world?\n1. Doctor (Smart and weak, interesting combo)\n2. Raider (otherwise know as a jerk)\n3. Soldier (feeling brave?)\n4. Janitor (Heart of gold and fists of steel)");
-    int selection = 5;
+    System.out.println("What would you like to be in this crazy new world?\n1. Doctor (Smart and weak, interesting combo)\n2. Raider (otherwise know as a jerk)\n3. Soldier (feeling brave?)\n4. Janitor (Heart of gold and fists of steel)\n5. Custom (who knows who you will be?");
+    int selection = 0;
 
-    while (selection > 4 || selection < 0)
+    while (selection > 5 || selection < 1)
     {
       try{
         selection = sc.nextInt();
@@ -76,9 +152,9 @@ public class App{
         sc.nextLine();
         System.out.println("Please enter an integer for character selection!");
       }
-      if (selection > 4 || selection < 0)
+      if (selection > 5 || selection < 1)
       {
-        System.out.println("Please select a number between 1 and 4!");
+        System.out.println("Please select a number between 1 and 5!");
       }
     }
     
@@ -101,7 +177,7 @@ public class App{
         player1.setStats();
         break;
       default:
-        player1 = new Janitor();
+        player1 = new Custom();
         player1.setStats();
         break;
     }
